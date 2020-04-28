579a947b3fbedb4eacfb9a63b338efcd1e1122d3
kaisawind <wind.kaisa@gmail.com>
2020-04-28 16:23:14 +0800

    add test

---
+++ 03.derive_eq/src/main.rs
@@ -0,0 +1,38 @@
+#[derive(PartialEq, Eq)]
+enum BookFormat {
+    Paperback,
+    Hardback,
+    Ebook,
+}
+
+#[derive(PartialEq, Eq)]
+struct Book {
+    isbn: i32,
+    format: BookFormat,
+}
+
+impl PartialEq<BookFormat> for Book {
+    fn eq(&self, other: &BookFormat) -> bool {
+        self.format == *other
+    }
+}
+
+impl PartialEq<Book> for BookFormat {
+    fn eq(&self, other: &Book) -> bool {
+        *self == other.format
+    }
+}
+
+fn main() {
+    let b1 = Book { isbn: 1, format: BookFormat::Paperback };
+    let b2 = Book { isbn: 2, format: BookFormat::Paperback };
+    let b3 = Book { isbn: 1, format: BookFormat::Ebook };
+
+    assert!(b1 == BookFormat::Paperback);
+    assert!(BookFormat::Paperback == b2);
+    assert!(b1 == b1);
+    assert!(b1 == b3);
+
+    // The following should hold by transitivity but doesn't.
+    assert!(b1 == b2); // <-- PANICS
+}
